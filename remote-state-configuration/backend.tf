52d91c0e12e8186cdf09be62feb0304505347580
Nikolay Kostov <35745114+nickkostov@users.noreply.github.com>
2024-11-04 11:46:03 +0200

    Update backend.tf

--- remote-state-configuration/backend.tf
+++ remote-state-configuration/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "mps-terraform-remote-states-nonprod"
-    prefix = "remote-state-configuration/terraform.tfstate"
+    bucket = ""
+    prefix = ""
   }
 }
